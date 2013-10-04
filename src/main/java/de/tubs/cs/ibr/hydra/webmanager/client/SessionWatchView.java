e4ba50dca68cb63d794d6133bf26e775e3b67d6f
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-10-04 16:35:24 +0200

    Add more data to the session watch view

--- src/main/java/de/tubs/cs/ibr/hydra/webmanager/client/SessionWatchView.java
+++ src/main/java/de/tubs/cs/ibr/hydra/webmanager/client/SessionWatchView.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.CellTable;
+import com.github.gwtbootstrap.client.ui.ProgressBar;
+import com.github.gwtbootstrap.client.ui.TextBox;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
@@ -27,6 +30,11 @@ public class SessionWatchView extends View {
     @UiField Button buttonBack;
     @UiField CellTable<Node> tableNodes;
     
+    @UiField TextBox textStatsState;
+    @UiField ProgressBar progressStats;
+    @UiField TextBox textStatsElapsedTime;
+    @UiField TextBox textStatsDesc;
+    
     // data provider for the node table
     ListDataProvider<Node> mDataProvider = new ListDataProvider<Node>();
     
@@ -37,15 +45,58 @@ public class SessionWatchView extends View {
 
     public SessionWatchView(HydraApp app, Session s) {
         super(app);
-        mSession = s;
-        
         initWidget(uiBinder.createAndBindUi(this));
         
         // create node table
         createNodeTable();
+
+        // initialize the session
+        refreshSession(s);
+    }
+    
+    private void refresh() {
+        if (mSession == null) return;
+        
+        // load session properties
+        refreshSession(mSession);
+    }
+    
+    private void update(Session s) {
+        // store data locally
+        mSession = s;
+
+        // update session state
+        textStatsState.setText(s.state.toString());
+        textStatsDesc.setText(s.name);
+        
+        // set progress bar to inifite if state is pending
+        if (Session.State.PENDING.equals(s.state)) {
+            progressStats.setPercent(100);
+        } else {
+            progressStats.setPercent(0);
+        }
         
         // load nodes
         refreshNodeTable(s);
+    }
+    
+    private void refreshSession(Session session) {
+        // check for null session objects
+        if (session == null) return;
+        
+        MasterControlServiceAsync mcs = (MasterControlServiceAsync)GWT.create(MasterControlService.class);
+        mcs.getSession(session.id, new AsyncCallback<Session>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                Window.alert("Can not get session properties.");
+                resetView();
+            }
+
+            @Override
+            public void onSuccess(Session result) {
+                update(result);
+            }
+        });
     }
     
     private void refreshNodeTable(Session s) {
@@ -88,16 +139,38 @@ public class SessionWatchView extends View {
             // do not update, if we don't have a session
             if (mSession == null) return;
             
-            for (EventExtra e : evt.getExtras()) {
-                if (EventType.EXTRA_SESSION_ID.equals(e.getKey())) {
-                    if (mSession.id.toString().equals(e.getData())) {
-                        // refresh nodes
-                        refreshNodeTable(mSession);
-                        return;
-                    }
+            if (isRelated(evt)) {
+                // refresh nodes
+                refreshNodeTable(mSession);
+            }
+        }
+        else if (EventType.SESSION_REMOVED.equals(evt)) {
+            if (isRelated(evt)) {
+                // close current view
+                resetView();
+            }
+        }
+        else if (EventType.SESSION_STATE_CHANGED.equals(evt)) {
+            if (isRelated(evt)) {
+                refresh();
+            }
+        }
+        else if (EventType.SESSION_DATA_UPDATED.equals(evt)) {
+            if (isRelated(evt)) {
+                refresh();
+            }
+        }
+    }
+    
+    private boolean isRelated(Event evt) {
+        for (EventExtra e : evt.getExtras()) {
+            if (EventType.EXTRA_SESSION_ID.equals(e.getKey())) {
+                if (mSession.id.toString().equals(e.getData())) {
+                    return true;
                 }
             }
         }
+        return false;
     }
 
     @UiHandler("buttonBack")
