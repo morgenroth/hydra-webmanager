06cc84a5bde09a0bdd9ad9c5d28fe8ad26dc7853
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-10-23 10:46:29 +0200

    SQL schema updated

--- hydra.sql
+++ hydra.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 17. Okt 2013 um 13:20
+-- Erstellungszeit: 23. Okt 2013 um 10:45
 -- Server Version: 5.5.32-0ubuntu0.13.04.1
 -- PHP-Version: 5.4.9-4ubuntu2.3
 
@@ -26,17 +26,20 @@ SET time_zone = "+00:00";
 -- Tabellenstruktur für Tabelle `nodes`
 --
 
+DROP TABLE IF EXISTS `nodes`;
 CREATE TABLE IF NOT EXISTS `nodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `slave` int(11) DEFAULT NULL,
+  `group` int(11) DEFAULT NULL,
   `assigned_slave` int(11) DEFAULT NULL,
   `session` int(11) NOT NULL,
   `name` varchar(256) DEFAULT NULL,
   `state` enum('draft','scheduled','created','connected','destroyed','error') NOT NULL DEFAULT 'draft',
   `address` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `address` (`address`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
+  UNIQUE KEY `address` (`address`),
+  KEY `group` (`group`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `nodes` (
 -- Tabellenstruktur für Tabelle `sessions`
 --
 
+DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL,
@@ -54,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `finished` datetime DEFAULT NULL,
   `state` enum('initial','draft','pending','running','finished','aborted','error','cancelled') NOT NULL DEFAULT 'initial',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4251 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,16 +66,17 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 -- Tabellenstruktur für Tabelle `slaves`
 --
 
+DROP TABLE IF EXISTS `slaves`;
 CREATE TABLE IF NOT EXISTS `slaves` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `address` varchar(64) DEFAULT NULL,
-  `state` enum('disconnected','idle','assigned','preparing','prepared','running','cleanup') NOT NULL DEFAULT 'disconnected',
+  `state` enum('disconnected','connected') NOT NULL DEFAULT 'disconnected',
   `owner` int(11) DEFAULT NULL,
   `capacity` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   KEY `owner` (`owner`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,6 +84,7 @@ CREATE TABLE IF NOT EXISTS `slaves` (
 -- Tabellenstruktur für Tabelle `stats`
 --
 
+DROP TABLE IF EXISTS `stats`;
 CREATE TABLE IF NOT EXISTS `stats` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `session` int(11) NOT NULL,
@@ -93,13 +99,14 @@ CREATE TABLE IF NOT EXISTS `stats` (
 -- Tabellenstruktur für Tabelle `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `passwd` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
