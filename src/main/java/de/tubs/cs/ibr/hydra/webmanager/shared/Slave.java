b8245cec57f986607116afe6863e2d46806c9559
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-10-23 14:48:43 +0200

    Fix comparison of two slave objects

--- src/main/java/de/tubs/cs/ibr/hydra/webmanager/shared/Slave.java
+++ src/main/java/de/tubs/cs/ibr/hydra/webmanager/shared/Slave.java
@@ -46,6 +46,10 @@ public class Slave implements IsSerializable {
     public Slave() {
     }
     
+    public Slave(Long id) {
+        this.id = id;
+    }
+    
     public Slave(String name, Long owner, Long capacity) {
         this.name = name;
         this.owner = owner;
@@ -62,8 +66,18 @@ public class Slave implements IsSerializable {
     public boolean equals(Object obj) {
         if (obj instanceof Slave) {
             Slave s = (Slave)obj;
+            if ((s.id != null) && (id != null)) return s.id == id;
             return (s.name == name) && (s.address == address);
         }
         return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return (name.hashCode() ^ address.hashCode());
+        } else {
+            return id.hashCode();
+        }
     }
 }
