87ce85a9e42e757ad5910f9fe32c244249e484fe
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-11-08 18:39:18 +0100

    Fix JSON access to stats data

--- src/main/java/de/tubs/cs/ibr/hydra/webmanager/client/stats/DtndInfoJso.java
+++ src/main/java/de/tubs/cs/ibr/hydra/webmanager/client/stats/DtndInfoJso.java
@@ -6,6 +6,6 @@ public class DtndInfoJso extends JavaScriptObject {
     protected DtndInfoJso() { }
     
     public final native int getNeighbors() /*-{ return this.Neighbors; }-*/;
-    public final native int getStorageSize() /*-{ return this.Storage-size; }-*/;
+    public final native int getStorageSize() /*-{ return this["Storage-size"]; }-*/;
     public final native int getUptime() /*-{ return this.Uptime; }-*/;
 }
